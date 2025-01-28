b97ae60d8aadfe06e37840086b95f0ca0e726293
vincreins <159515976+vincreins@users.noreply.github.com>
2025-01-28 20:51:58 +0545

    Update

--- code/combine_regime_results.R
+++ code/combine_regime_results.R
@@ -1,9 +1,9 @@
 combine_regime_results <- function(results_mv, results_RPP, results_RPP_nb, results_md) {
 
-    results_mv$Dataset <- "MV"
+    results_mv$Dataset <- "MVP"
     results_RPP$Dataset <- "RPP"
     results_RPP_nb$Dataset <- "RPP_nb"
-    results_md$Dataset <- "MD"
+    results_md$Dataset <- "MDP"
 
     combined_results <- rbind(results_mv, results_RPP, results_RPP_nb, results_md)
 
